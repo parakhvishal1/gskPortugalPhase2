9720f5d87475432f6a0131aee98e0e5a66227628
parakhvishal1 <66335608+parakhvishal1@users.noreply.github.com>
2023-12-19 16:36:36 +0530

    Update parentIframe_pt.js added more info

--- assets/js/parentIframe_pt.js
+++ assets/js/parentIframe_pt.js
@@ -90,7 +90,7 @@ function injectDynamicCssToParent() {
 window.addEventListener('message', function (eventData) {
     // console.error('eventData', eventData);
    // if(eventData.origin !== 'https://r5.cloud.yellow.ai') return;
-    console.log("eventDataVishal19932023",eventData);
+    console.log("eventDataVishal19932023",eventData.data);
     let parsedData = JSON.parse(eventData.data);
 
     console.log("parsedData", parsedData)
