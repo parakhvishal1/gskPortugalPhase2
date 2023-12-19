8322428aea577d99fffb7e64f5298942b0df9de2
parakhvishal1 <66335608+parakhvishal1@users.noreply.github.com>
2023-12-19 16:27:22 +0530

    Update parentIframe_pt.js log added at line 93

--- assets/js/parentIframe_pt.js
+++ assets/js/parentIframe_pt.js
@@ -90,6 +90,7 @@ function injectDynamicCssToParent() {
 window.addEventListener('message', function (eventData) {
     // console.error('eventData', eventData);
    // if(eventData.origin !== 'https://r5.cloud.yellow.ai') return;
+    console.log("eventDataVishal19932023",eventData);
     let parsedData = JSON.parse(eventData.data);
 
     console.log("parsedData", parsedData)
